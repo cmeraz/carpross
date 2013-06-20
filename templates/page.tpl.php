2d8031f6c9f610c16cc9db5056afd0e10a580e23
Carlos Meraz <cmeraz3944@gmail.com>
2013-06-19 21:29:36 -0500

    Updated page.tpl.php and some theme.css for styling frontpage

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -99,6 +99,11 @@
 <!-- Page Container -->
 <div class="container">
  	<div class="row">
+    <?php if ($page['sidebar_first']): ?>
+      <aside class="span3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>    
     <section class="span9">  
       <?php if ($tabs): ?>
         <?php print render($tabs); ?>
@@ -111,58 +116,5 @@
       <?php endif; ?>
       <?php print render($page['content']); ?>
     </section>
-	  <?php if ($page['sidebar_first']): ?>
-      <aside class="span3" role="complementary">
-  		  <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>    
   </div> 
 </div>
-
-<!-- Footer section of page -->
-<footer class="background">
-  <div class="container">
-    <div class="row">
-      <div class="span12">
-      	<p>© 2013 Designed by <a href="http://twitter.com/heyallanmoreno">@heyallanmoreno</a></p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="span12">
-        <?php print render($page['footer']); ?>
-      </div>
-    </div>
-  </div>
-</footer>
-
-<script>
-
-  $(document).ready(function(){
-
-	  // fancy animation - fade out header content on page scroll
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('.js-animated').stop().animate({opacity: 0}, 800);
-      }
-      if ($(this).scrollTop() < 100) {
-        $('.js-animated').stop().animate({opacity: 1}, 500);
-      }
-
-      //Navigation bar fixed
-      if ($(this).scrollTop() > $('div.showcase').height()) {
-        $('body').addClass('fixed');
-        $('.strip').addClass('navbar-fixed-top').stop().animate({opacity: 0.95}, 800);
-        
-      }
-      if ($(this).scrollTop() < $('div.showcase').height()) {
-      	$('body').removeClass('fixed');
-        $('.strip').removeClass('navbar-fixed-top').stop().animate({opacity: 1}, 500);
-      }
-
-    });
-
-    //bootstrap tooltip trigger
-    $('[rel="tooltip"]').tooltip();
-  });
-
-</script>
