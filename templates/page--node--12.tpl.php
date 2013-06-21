0008c44d201ad63221cbb3354d419eaaa0114fa1
Carlos Meraz <cmeraz3944@gmail.com>
2013-06-20 19:56:00 -0500

    Updated page--node--12.tpl.php changing the footer section

--- templates/page--node--12.tpl.php
+++ templates/page--node--12.tpl.php
@@ -4,26 +4,29 @@
     <div class="row">
       <div class="span12">
         <header style="opacity: 1;" class="js-animated main navbar-fixed-top">
-
           <!-- Site's Logo -->
           <?php if ($logo): ?>
             <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
               <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
             </a>
           <?php endif; ?>
-  
         </header>
-        <footer class="navbar-fixed-bottom pull-center">
-          
-          <h1 class="text-center">OOPS <span>PAGINA NO ENCONTRADA!</span> </h1>
-          <p class="lead text-center">Lo sentimos, la pagina que busca no se encuentra en el sistema o ha sido cambiada</p>
-          <a class="btn bnt-primary btn-large pull-center" href="/" title="Volver a la pagina de inicio">
-            <i class="icon-home"> Volver al Inicio</i>
-          </a>
-        </footer>
       </div>
     </div>
   </div>
+  <footer class="navbar-fixed-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <h1 class="text-center pull-center">OOPS <span>PAGINA NO ENCONTRADA!</span> </h1>
+          <p class="lead text-center pull-center">Lo sentimos, la pagina que busca no se encuentra en el sistema o ha sido cambiada</p>
+          <a class="btn bnt-primary btn-large pull-center" href="/" title="Volver a la pagina de inicio">
+            <i class="icon-home"> Volver al Inicio</i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </div>
 
 
